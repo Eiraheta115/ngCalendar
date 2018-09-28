b0e884d54492a3b8da69bb48141e09f7eb99378e
EIraheta115 <ceiraheta87@gmail.com>
2018-09-27 18:53:25 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
-
+import { CalendarComponent } from 'ng-fullcalendar';
+import { Options } from 'fullcalendar';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testFullcalendar';
+
+  //events: any[];
+  title = 'my-angular-app';
+  calendarOptions: Options;
+  ngOnInit() {
+  this.calendarOptions = {
+        editable: true,
+        eventLimit: false,
+        header: {
+          left: 'prev,next today',
+          center: 'title',
+          right: 'month,agendaWeek,agendaDay,listMonth'
+        },
+        selectable: true,
+        events : [
+          {
+              "title": "All Day Event",
+              "start": "2018-09-01"
+          },
+          {
+              "title": "Decirle a mi osita que es hermosa",
+              "start": "2018-09-07",
+              "end": "2018-09-10"
+          },
+          {
+              "title": "Repeating Event",
+              "start": "2018-09-09T16:00:00"
+          },
+          {
+              "title": "Repeating Event",
+              "start": "2016-01-16T16:00:00"
+          },
+          {
+              "title": "Conference",
+              "start": "2016-01-11",
+              "end": "2016-01-13"
+          }
+      ]
+        
+      };
+      
+  }
 }
